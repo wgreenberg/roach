a2d347dfc33c8d0c2f86219e1367788edc850b3a
Will Greenberg <willg@eff.org>
2020-11-07 15:29:49 -0800

    use longer game for benchmarks

--- benches/sgf_benchmark.rs
+++ benches/sgf_benchmark.rs
@@ -1,8 +1,8 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use hive::sgf_parser::read_sgf_file;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("random game", |b| b.iter(|| read_sgf_file("./test_data/HV-jimthelynx-biboelmo-2020-03-28-2235.sgf")));
+    c.bench_function("random game", |b| b.iter(|| read_sgf_file("./test_data/T!HV-stepanzo-tzimarou-2020-07-31-0524.sgf")));
 }
 
 criterion_group!(benches, criterion_benchmark);
