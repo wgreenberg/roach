5bd1d754b6a0bc3d52cdfe30c1dc019e95cb3cb9
Will Greenberg <willg@eff.org>
2020-11-25 17:18:37 -0800

    roach: add warp hello world

--- roach/src/main.rs
+++ roach/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+use tokio;
+use warp::{http::StatusCode, Filter};
+
+mod arena;
+mod hive_match;
+mod matchmaker;
+mod player;
+mod ws;
+
+#[tokio::main]
+async fn main() {
+    let health_route = warp::path("health")
+        .map(|| StatusCode::OK);
+
+    let routes = health_route
+        .with(warp::cors().allow_any_origin());
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
