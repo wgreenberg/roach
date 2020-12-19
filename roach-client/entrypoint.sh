d0f28097ea1adbe0a8db0fead22b75609e31f88b
Will Greenberg <willg@eff.org>
2020-12-18 19:25:10 -0800

    make clients run foreeeeeveerrr

--- roach-client/entrypoint.sh
+++ roach-client/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -x
 
-roach-client -b cli-engine -a "-n \"$N_ITERATIONS\" -d \"$MAX_DEPTH\"" matchmaking -s https://roach.rodeo -t "$API_TOKEN"
+while 1; do
+    roach-client -b cli-engine -a "-n \"$N_ITERATIONS\" -d \"$MAX_DEPTH\"" matchmaking -s https://roach.rodeo -t "$API_TOKEN"
+done
